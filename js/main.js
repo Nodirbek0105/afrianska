1c266cd54538a17c6e4fdf6486d8b59de7ce0d88
-m <-m>
2022-12-20 14:02:31 +0500

    uydhjgbniurjkv

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var elul = document.querySelector("[data-ul]")
-var ellabel = document.querySelector("[data-label]")
-var ellabel2 = document.querySelector("[data-label2]")
-var elimg = document.querySelector("[data-img]")
+var elul = document.querySelector("[data-ul]");
+var ellabel = document.querySelector("[data-label]");
+var ellabel2 = document.querySelector("[data-label2]");
+var elimg = document.querySelector("[data-img]");
+// var eldiv = document.querySelector("[data-container]");
 
+ellabel.addEventListener("click", () => {
+  ellabel.classList.add("visually");
+  elul.classList.toggle("hamburger-list");
+});
 
-ellabel.addEventListener("click" , ()=>{
-  ellabel.classList.toggle("visually")
-  elul.classList.toggle("hamburger-list")
-})
-
-ellabel2.addEventListener("click" , ()=>{
-  ellabel.classList.remove("visually")
-  elul.classList.toggle("hamburger-list")
-})
+ellabel2.addEventListener("click", () => {
+  ellabel.classList.remove("visually");
+  elul.classList.toggle("hamburger-list");
+});
